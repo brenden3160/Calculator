e71fcc51f8bdbbbdd71bd6d86d8953744dd9855d
brenden3160 <43614374+brenden3160@users.noreply.github.com>
2019-04-18 17:37:44 -0400

    Update BMI.java

--- Calculator/src/BMI.java
+++ Calculator/src/BMI.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
-public class BMI {
+
+/**
+ * This is class to calculate body mass index.
+ * 
+ * @author rbrathgeber1940 Brenden Rathgeber made this.
+ */
+public class BodyMassIndex {
   private double height;
   private double weight;
 
@@ -12,21 +18,25 @@ public class BMI {
     this.weight = weight;
   }
 
-  public double getBmi() {
+  public double getBodyMassIndex() {
     return Math.abs((weight * 703) / (Math.pow(height, 2)));
   }
 
-  public static void findBmi(Scanner scanner) {
-    BMI myBMI = new BMI();
+
+  /**
+   * This is the code for prompting user and giving back info.
+   * 
+   * @param scanner Scanner takes height and weight.
+   */
+  public static void findBodyMassIndex(Scanner scanner) {
+    BodyMassIndex myBodyMassIndex = new BodyMassIndex();
     boolean goodInput = false;
     while (goodInput == false) {
       try {
         System.out.println("Please enter your height in inches:");
-        myBMI.setHeight(scanner.nextDouble());
+        myBodyMassIndex.setHeight(scanner.nextDouble());
         goodInput = true;
-      }
-
-      catch (Exception ex) {
+      } catch (Exception ex) {
         System.out.println("Invalid input. Please try again.");
         scanner.nextLine();
       }
@@ -35,22 +45,21 @@ public class BMI {
     boolean goodInput2 = false;
     while (goodInput2 == false) {
       try {
-        System.out.println("Please enter your weight in pounds (yes ladies, you too...)");
-        myBMI.setWeight(scanner.nextDouble());
+        System.out.println("Please enter your weight in pounds");
+        myBodyMassIndex.setWeight(scanner.nextDouble());
         goodInput2 = true;
-      }
-
-      catch (Exception ex) {
+      } catch (Exception ex) {
         System.out.println("Invalid input. Please try again.");
         scanner.nextLine();
       }
     }
 
-    System.out.println("Your BMI is " + myBMI.getBmi());
+    System.out.println("Your BMI is " + myBodyMassIndex.getBodyMassIndex());
 
-    if (myBMI.getBmi() < 18.5) {
+    if (myBodyMassIndex.getBodyMassIndex() < 18.5) {
       System.out.println("According to your BMI, you are probably underweight.");
-    } else if (myBMI.getBmi() > 18.5 && myBMI.getBmi() < 24.9) {
+    } else if (myBodyMassIndex.getBodyMassIndex() > 18.5
+        && myBodyMassIndex.getBodyMassIndex() < 24.9) {
       System.out.println("According to your BMI, you are at a normal weight.");
     } else {
       System.out.println("According to your BMI, you are most likely overweight.");
