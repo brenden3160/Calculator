efeb9dd3f505ed1d662994f067cb4cdc6eccb08b
rbrathgeber1940 <rbrathgeber1940@H203P006.STUDENT.ad.fgcu.edu>
2019-04-18 14:17:05 -0400

    new code new project

---
+++ Calculator/src/Cube.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Cube extends NormalPolygon {
+  // field declarations
+
+  public double getVolume() {
+    return Math.abs(Math.pow(sideLength, 3));
+  }
+
+  public double getSurfaceArea() {
+    return Math.abs(Math.pow(sideLength, 2) * 6);
+  }
+
+  public static void measureCube(Scanner scanner) {
+    Cube myCube = new Cube();
+    boolean goodInput = false;
+    while (goodInput == false) {
+      try {
+        System.out.println("Please enter a side length of the square below:");
+        myCube.setSideLength(scanner.nextDouble());
+        goodInput = true;
+      }
+
+      catch (Exception ex) {
+        System.out.println("Invalid input. Please try again.");
+        scanner.nextLine();
+      }
+    }
+    System.out.println("\nCube Dimensions");
+    System.out.println("Perimeter: " + myCube.getVolume() + " units cubed.");
+    System.out.println("Diagonal Distance Across: " + myCube.getSurfaceArea() + " units squared.");
+  }
+}
---
+++ Calculator/src/HackRussia.java
@@ -0,0 +1,10 @@
+/*import java.util.Scanner;
+
+public class HackRussia {
+	private String RussiaPassword;
+	
+	 public void setRussiaPassword(String RussiaPassword) {
+		    this.RussiaPassword = RussiaPassword;
+		  }
+}
+*/
